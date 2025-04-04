6cc7771ec5e5870cf788d882bf403770a7e9f50f
e-MKH <167267400+e-MKH@users.noreply.github.com>
2025-04-04 12:35:34 +0900

    reCreate
    Templete : Empty Activity

--- app/src/test/java/com/example/capstone/ExampleUnitTest.kt
+++ app/src/test/java/com/example/capstone/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.project
+package com.example.capstone
 
 import org.junit.Test
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,5 +19,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Project"
+rootProject.name = "capstone"
 include(":app")
